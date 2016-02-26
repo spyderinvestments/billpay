0f8875dcca6ff38827aabdc394db6bac5631ee88
Michael Liang <michaelpliang@gmail.com>
2016-02-25 17:50:49 -0800

    angular bootstrap added

--- src/javascripts/module.js
+++ src/javascripts/module.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var app = angular.module('mainApp', ['ui.router']);
+var app = angular.module('mainApp', ['ui.router', 'ngAnimate', 'ui.bootstrap']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider
-    .state('home', { url: '/', templateUrl: '/templates/home.html' })
+    .state('home', { url: '/', templateUrl: '/templates/home.html', controller: 'homeCtrl' })
     .state('login', { url: '/login', templateUrl: './templates/login.html', controller: 'loginCtrl' })
     .state('dashboard', { url: '/dashboard', templateUrl: './templates/dashboard.html', controller: 'dashboardCtrl' })
 
